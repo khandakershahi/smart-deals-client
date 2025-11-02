ce669ee31228992946e5517285d7edeb39bd8b31
khandakershahi <khandakershahi@gmail.com>
2025-11-03 01:30:12 +0600

    lots of edit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,52 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+import Home from './components/Home/Home.jsx';
+import AllProducts from './components/AllProducts/AllProducts.jsx';
+
+import Register from './components/Register/Register.jsx';
+import RootLayout from './Layouts/RootLayout.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
+import MyProducts from './components/MyProducts/MyProducts.jsx';
+import MyBids from './components/MyBids/MyBids.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'allProducts',
+        Component: AllProducts
+      }, 
+      {
+        path: 'register',
+        Component: Register
+      }, 
+      {
+        path: 'myProducts',
+        element: <MyProducts></MyProducts>
+      },
+      {
+        path: 'myBids',
+        element: <MyBids></MyBids>
+      }
+     
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
